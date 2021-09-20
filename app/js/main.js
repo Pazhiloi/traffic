68dce8fdd997fcc7f5e47245fc5ab51090409b59
Pazhiloi <martinsen06@gmail.com>
2021-09-20 20:59:17 +0300

    fixed adaptive and scripts

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ window.onscroll = function showHeader() {
     }
    }
 
-$(function () {
+ $(function () {
   $('.carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -16,7 +16,18 @@ $(function () {
     centerPadding: '0px', 
      prevArrow:'<button type="button" class="results__prev"><svg width="14" height="24" viewBox="0 0 14 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.93934 10.9393C0.353553 11.5251 0.353553 12.4749 0.93934 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97919 12.6066 1.3934C12.0208 0.807612 11.0711 0.807612 10.4853 1.3934L0.93934 10.9393ZM4 10.5L2 10.5L2 13.5L4 13.5L4 10.5Z" fill="white" /></svg></button>',
     nextArrow: '<button type="button" class="results__next"><svg width="14" height="24" viewBox="0 0 14 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.93934 10.9393C0.353553 11.5251 0.353553 12.4749 0.93934 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97919 12.6066 1.3934C12.0208 0.807612 11.0711 0.807612 10.4853 1.3934L0.93934 10.9393ZM4 10.5L2 10.5L2 13.5L4 13.5L4 10.5Z"fill="white" /></svg></button>',
-    infinite: false
+    infinite: false,
+    responsive: [
+    {
+      breakpoint: 770,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+
   });
   $('.review__slider').slick({
     slidesToShow: 1,
@@ -44,14 +55,14 @@ const dropDownBtn = document.querySelector('.vacancies__title'),
       faqItems = document.querySelectorAll('.faq__item'),
       enter = document.querySelectorAll('.enter'),
       signup = document.querySelector('.top__button'),
-      popupLog = document.querySelector('.popup-log'),
-      popupReg = document.querySelector('.popup-reg'),
+      popupLog = document.getElementById('popup-log'),
+      popupReg = document.getElementById('popup-reg'),
       lazyImages = document.querySelectorAll('img[data-src]'),
       windowHeight = document.documentElement.clientHeight;
 
 
 
-       store.addEventListener('click', () =>{
+      store.addEventListener('click', () =>{
         storeWrapper.classList.toggle('active');
       });
        let lazyImagesPosition = [];
@@ -84,58 +95,33 @@ const dropDownBtn = document.querySelector('.vacancies__title'),
           } 
           delete lazyImagesPosition[imgIndex];
         }
-      };   
+      };
        faqItems.forEach(item => {
         item.addEventListener('click', () => {
           item.classList.toggle('active');
         })
-      });  
-      try{
-         cardBtn.forEach(btn => {
+      });   
+       cardBtn.forEach(btn => {
         btn.addEventListener('click', () =>{
           btn.classList.add('active');
         })
       });
      
       menuBtn.addEventListener('click', () => {
-        menuList.classList.toggle('menu__list--active')
+        menuBtn.classList.toggle('active');
+        menuList.classList.toggle('menu__list--active');
       });
       dropDownBtn.addEventListener('click', () =>{
         dropDownWrapper.classList.toggle('active');
         dropDownBtn.classList.toggle('active');
       });
-      
-     coursesPreview.forEach(item => {
-        item.addEventListener('click', () =>{
-          item.classList.toggle('active');
-        })
-      });
-         coursesWrapper.addEventListener('click', () => {
-        coursesWrapper.classList.toggle('active');
-        coursesListWrapper.classList.toggle('active');
-      });
-      
-        aboutBtn.addEventListener('click', () => {
-        aboutForm.classList.add('active');
-        aboutBtn.classList.add('hide');
-      });
-  
-      aboutFormBtn.addEventListener('click', () => {
-        aboutForm.classList.remove('active');
-        aboutBtn.classList.remove('hide');
-      });
-        
-      enter.forEach(enterBtn => {
+       enter.forEach(enterBtn => {
          enterBtn.addEventListener('click', (e) =>{
          e.preventDefault();
          openModal(popupLog);
        } );
       });
-        signup.addEventListener('click', (e) =>{
-         e.preventDefault();
-         openModal(popupReg);
-         ;
-       } );
+      
       
        function closeModalListener(selector) {
          selector.addEventListener('click', (e) => {
@@ -160,8 +146,34 @@ const dropDownBtn = document.querySelector('.vacancies__title'),
        modalSelector.classList.remove('active');
        document.body.style.overflow = '';
      }
+     
       
-      }catch {console.log('error')}
-
       
+     coursesPreview.forEach(item => {
+        item.addEventListener('click', () =>{
+          item.classList.toggle('active');
+        })
+      });
+         coursesWrapper.addEventListener('click', () => {
+        coursesWrapper.classList.toggle('active');
+        coursesListWrapper.classList.toggle('active');
+      });
+      
+        aboutBtn.addEventListener('click', () => {
+        aboutForm.classList.add('active');
+        aboutBtn.classList.add('hide');
+      });
+  
+      aboutFormBtn.addEventListener('click', () => {
+        aboutForm.classList.remove('active');
+        aboutBtn.classList.remove('hide');
+      });  
+      try {
+        signup.addEventListener('click', (e) =>{
+         e.preventDefault();
+        openModal(popupReg);
+       } );
+      } catch (error) {
+        console.log('error');
+      }
    });
